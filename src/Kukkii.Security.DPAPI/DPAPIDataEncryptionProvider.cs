27508f7620e4cc63280d9ff829e49d1d0be30aa4
Amrykid <amrykid@gmail.com>
2014-03-28 17:39:01 -0500

    Started a Kukkii.Security.DPAPI project. ProtectedData isn't supported in PCL so it will be a while before its used.

---
+++ src/Kukkii.Security.DPAPI/DPAPIDataEncryptionProvider.cs
@@ -0,0 +1,22 @@
+﻿using Kukkii.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kukkii.Security.DPAPI
+{
+    public class DPAPIDataEncryptionProvider: ICookieDataEncryptionProvider
+    {
+        public byte[] EncryptData(byte[] unencryptedData)
+        {
+            //System.Security.ProtectedData
+            throw new NotImplementedException();
+        }
+
+        public byte[] DecryptData(byte[] encryptedData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
