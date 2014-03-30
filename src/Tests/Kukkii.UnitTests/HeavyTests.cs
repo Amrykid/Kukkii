6e018e66b162a2226c100a4a679febd994e3ec7a
Amrykid <amrykid@gmail.com>
2014-03-29 23:13:51 -0500

    Heavier tests.

---
+++ src/Tests/Kukkii.UnitTests/HeavyTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kukkii.UnitTests
+{
+    public class HeavyTests
+    {
+        [Fact]
+        public async Task InsertLotsOfRandomObjectsAndWaitAndGetOneOfThemAsync()
+        {
+            var tasks = new List<Task>();
+            for (int i = 0; i < 20000; i++)
+            {
+                tasks.Add(
+                    CookieJar.InMemory.InsertObjectAsync("Object" + i.ToString(), i * 2));
+            }
+
+            await Task.WhenAll(tasks.ToArray());
+
+            Assert.Equal(26 * 2, (await CookieJar.InMemory.GetObjectAsync<int>("Object26")));
+
+            Utilities.ForceResetCookieJar();
+        }
+    }
+}
