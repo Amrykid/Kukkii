987f1907c7ad42b410cc31e08935e553520ba336
Amrykid <amrykid@gmail.com>
2014-04-23 09:26:39 -0500

    New thread handling has a trade off. Simple operations take slightly longer but it is no longer hogging the CPU. This could be a battery killer on mobile platforms.

--- src/Tests/Kukkii.UnitTests/HeavyTests.cs
+++ src/Tests/Kukkii.UnitTests/HeavyTests.cs
@@ -25,23 +25,23 @@ namespace Kukkii.UnitTests
 
             Utilities.ForceResetCookieJar();
         }
-        [Fact]
-        public async Task InsertLotsOfRandomObjectsAndWaitFor5MinutesAndGetOneOfThemAsync()
-        {
-            var tasks = new List<Task>();
-            for (int i = 0; i < 20000; i++)
-            {
-                tasks.Add(
-                    CookieJar.InMemory.InsertObjectAsync("Object" + i.ToString(), i * 2));
-            }
+        //[Fact]
+        //public async Task InsertLotsOfRandomObjectsAndWaitFor5MinutesAndGetOneOfThemAsync()
+        //{
+        //    var tasks = new List<Task>();
+        //    for (int i = 0; i < 20000; i++)
+        //    {
+        //        tasks.Add(
+        //            CookieJar.InMemory.InsertObjectAsync("Object" + i.ToString(), i * 2));
+        //    }
 
-            await Task.WhenAll(tasks.ToArray());
+        //    await Task.WhenAll(tasks.ToArray());
 
-            await Task.Delay(60000 * 5);
+        //    await Task.Delay(60000 * 5);
 
-            Assert.Equal(26 * 2, (await CookieJar.InMemory.GetObjectAsync<int>("Object26")));
+        //    Assert.Equal(26 * 2, (await CookieJar.InMemory.GetObjectAsync<int>("Object26")));
 
-            Utilities.ForceResetCookieJar();
-        }
+        //    Utilities.ForceResetCookieJar();
+        //}
     }
 }
